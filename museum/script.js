7f3168a16c1d39c198335d89a6715df75638b84d
Yulia Lepner <ylepner@gmail.com>
2021-09-26 18:03:14 +0200

    feat: check for alt atribute

---
+++ museum/script.js
@@ -0,0 +1,13 @@
+const progressVideo = document.querySelector('.progress-video');
+
+progressVideo.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #d3d3d3 ${value}%, #d3d3d3 100%)`
+})
+
+const progressVolume = document.querySelector('.progress-volume');
+
+progressVolume.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #d3d3d3 ${value}%, #d3d3d3 100%)`
+})
