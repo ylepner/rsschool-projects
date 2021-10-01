0f978c54f44f50dd232750a69f5deee2e6a57449
Yulia Lepner <ylepner@gmail.com>
2021-10-01 12:10:53 +0200

    init: start museum-adaptive

---
+++ museum_adaptive/script.js
@@ -0,0 +1,14 @@
+console.log()
+const progressVideo = document.querySelector('.progress-video');
+
+progressVideo.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #d3d3d3 ${value}%, #d3d3d3 100%)`
+})
+
+const progressVolume = document.querySelector('.progress-volume');
+
+progressVolume.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #d3d3d3 ${value}%, #d3d3d3 100%)`
+})
