a6902724d89b7bcc3150d653c3785c204e6aa2aa
Yulia Lepner <ylepner@gmail.com>
2021-10-09 12:55:52 +0200

    reafactor: add welcome-img

--- museum-dom/script.js
+++ museum-dom/script.js
@@ -1,3 +1,62 @@
+/* slide */
+
+const prev = document.getElementById('btn-prev'),
+  next = document.getElementById('btn-next'),
+  slides = document.querySelectorAll('.slide')
+dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+const activeSlide = n => {
+  for (let slide of slides) {
+    slide.classList.remove('active')
+  }
+  slides[n].classList.add('active');
+}
+
+const activeDot = n => {
+  for (let dot of dots) {
+    dot.classList.remove('active')
+  }
+  dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind => {
+  activeSlide(ind);
+  activeDot(ind);
+}
+
+const nextSlide = () => {
+  if (index == slides.length - 1) {
+    index = 0;
+    prepareCurrentSlide(index);
+  } else {
+    index++;
+    prepareCurrentSlide(index);
+  }
+}
+const prevSlide = () => {
+  if (index == 0) {
+    index = slides.length - 1;
+    activeSlide(index)
+  } else {
+    index--;
+    activeSlide(index);
+  }
+}
+
+next.addEventListener('click', nextSlide());
+prev.addEventListener('click', prevSlide());
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    index = indexDot;
+    prepareCurrentSlide(index);
+  })
+})
+
+/* video  */
+
 const progressVideo = document.querySelector('.progress-video');
 
 progressVideo.addEventListener('input', function () {
