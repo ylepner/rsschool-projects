f0c3d50b4dce8c96e21b39acf1bfdb47adfa83c6
Yulia Lepner <ylepner@gmail.com>
2021-10-09 20:45:33 +0200

    feat: slider in explore section

--- museum-dom/script.js
+++ museum-dom/script.js
@@ -1,5 +1,6 @@
+/* slider */
 
-var slider = tns({
+const slider = tns({
   container: '.my-slider',
   items: 1,
   slideBy: 'page',
@@ -15,6 +16,33 @@ slider.events.on('indexChanged', function(sliderInfo){
   console.log(sliderInfo.index)
   document.getElementById('img-index').innerText = '0' + sliderInfo.index
 });
+
+
+
+const bar = document.getElementsByClassName('explore-img-slider-bar')[0]
+let isDragging = false
+bar.addEventListener('mousedown', (evt)=> {
+  bar.style['pointer-events'] = 'none'
+  isDragging = true
+});
+
+const canvas = document.getElementsByClassName('explore-img')[0]
+const r = document.querySelector(':root');
+canvas.addEventListener('mousemove', (evt)=> {
+  if (!isDragging) {
+    return
+  }
+  console.log(evt)
+  r.style.setProperty('--image-compare-slider-position', evt.offsetX+'px');
+})
+
+document.addEventListener('mouseup', ()=> {
+  isDragging = false
+  bar.style['pointer-events'] = ''
+})
+
+
+
 
 /* video  */
 
