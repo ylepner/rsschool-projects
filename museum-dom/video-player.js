f7761afcaf3455187c27d9eb66547e46a8733583
Yulia Lepner <ylepner@gmail.com>
2021-10-10 23:09:51 +0200

    feat: add play button

--- museum-dom/video-player.js
+++ museum-dom/video-player.js
@@ -1,11 +1,20 @@
 const player = document.querySelector('.player')
 const bigPlayButton = player.querySelector('.player-btn-play-big')
 const video = player.querySelector('video')
+const smallPlayButton = player.querySelector('.button-small-play')
+const smallPauseButton = player.querySelector('.button-small-pause')
 
 
-bigPlayButton.onclick = () => {
+bigPlayButton.onclick = play
+smallPauseButton.onclick = stop
+
+function play() {
   video.play()
   player.classList.add('is-playing')
 }
 
+function stop() {
+  video.pause()
+  player.classList.remove('is-playing')
+}
 
