778409edbe94a25f487096fec41967409bbb43fb
Yulia Lepner <ylepner@gmail.com>
2021-10-10 18:29:27 +0200

    feat: add animation to gallery, start video slider

--- museum-dom/script.js
+++ museum-dom/script.js
@@ -7,41 +7,38 @@ const slider = tns({
   controlsContainer: ".welcome-slider-arrows",
   navContainer: ".welcome-slider-button",
   mouseDrag: true,
-  center:true,
+  center: true,
 });
 
 
-
-slider.events.on('indexChanged', function(sliderInfo){
+slider.events.on('indexChanged', function (sliderInfo) {
   console.log(sliderInfo.index)
   document.getElementById('img-index').innerText = '0' + sliderInfo.index
 });
 
-
+/* slider explore */
 
 const bar = document.getElementsByClassName('explore-img-slider-bar')[0]
 let isDragging = false
-bar.addEventListener('mousedown', (evt)=> {
+bar.addEventListener('mousedown', (evt) => {
   bar.style['pointer-events'] = 'none'
   isDragging = true
 });
 
 const canvas = document.getElementsByClassName('explore-img')[0]
 const r = document.querySelector(':root');
-canvas.addEventListener('mousemove', (evt)=> {
+canvas.addEventListener('mousemove', (evt) => {
   if (!isDragging) {
     return
   }
   console.log(evt)
-  r.style.setProperty('--image-compare-slider-position', evt.offsetX+'px');
+  r.style.setProperty('--image-compare-slider-position', evt.offsetX + 'px');
 })
 
-document.addEventListener('mouseup', ()=> {
+document.addEventListener('mouseup', () => {
   isDragging = false
   bar.style['pointer-events'] = ''
 })
-
-
 
 
 /* video  */
@@ -63,5 +60,146 @@ progressVolume.addEventListener('input', function () {
 document.getElementById("menu-toggle").onclick = function () {
   document.body.classList.toggle("mobile-menu-close")
   document.body.classList.toggle("mobile-menu-open")
+}
+
+/* slider video */
+
+const sliderVideo = tns({
+  container: '.video-carousel',
+  items: 3,
+  slideBy: 'page',
+  controlsContainer: ".video-slider",
+  /*  prevButton: ".prev-button",
+   nextButton: ".next-button", */
+  navContainer: ".video-slider-dot",
+  center: true,
+});
+
+document.querySelector('.prev-button').onclick = function () {
+  sliderVideo.goTo('prev');
+};
+
+document.querySelector('.next-button').onclick = function () {
+  sliderVideo.goTo('next');
+};
+
+// stop video
+
+sliderVideo.events.on('indexChanged', function (sliderInfo) {
+  Array.from(document.getElementsByClassName('video-carousel')[0].querySelectorAll('iframe')).forEach(iframe => {
+    iframe.contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+  })
+});
+
+/* function stopVideo(el) {
+  const iframe = element.querySelector('iframe');
+  const video = element.querySelector('video');
+  if (iframe) {
+    const iframeSrc = iframe.src;
+    iframe.src = iframeSrc;
+  }
+  if (video) {
+    video.pause();
+  }
+}
+ */
+
+
+
+
+/* validation */
+
+const form = document.getElementById('form')
+const date = document.getElementById('date')
+const time = document.getElementById('time')
+const userName = document.getElementById('userName')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const ticket = document.getElementById('ticket')
+
+form.addEventListener('submit', (e) => {
+  console.log(e)
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  const daeValue = date.value.trim()
+  const timeValue = time.value.trim()
+  const userNameValue = userName.value.trim()
+  const emailValue = email.value.trim()
+  const phoneValue = phone.value.trim()
+  const ticketValue = ticket.value.trim()
+
+  if (userNameValue === '') {
+    setErrorFor(userName, 'Name cannot be blank')
+  } else {
+    setSuccessFor(userName)
+  }
+  if (email === '') {
+    setErrorFor(email, 'Email cannot be blank');
+  } else if (!isEmail(emailValue)) {
+    setSuccessFor(email, 'Email is not valid')
+  } else {
+    setSuccessFor(email);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement
+  const small = formControl.querySelector('small')
+  small.innerText = message;
+
+  formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success'
+}
+
+/* function isEmail(email) {
+  return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}
+  [a - zA - Z0 - 9])?(?: \.[a - zA - Z0 - 9](?: [a - zA - Z0 - 9 -]{ 0, 61}[a - zA - Z0 - 9]) ?)* $ /.test(email)
+} */
+
+
+/* gallery */
+
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll)
+  function animOnScroll(params) {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffSet = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((scrollY > animItemOffSet - animItemPoint) && scrollY < (animItemOffSet + animItemHeight)) {
+        animItem.classList.add('active');
+      } else {
+        animItem.classList.remove('active')
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+      scrollTop = window.scrollY || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+
+  setTimeout(() => {
+    animOnScroll();
+  }, 300);
+
 }
 
