3d17334dc16c1d432f5fd83ed78ac9368aca1914
Yulia Lepner <ylepner@gmail.com>
2021-10-11 12:55:55 +0200

    refactor: divide js scrpit

---
+++ museum-dom/explore.js
@@ -0,0 +1,21 @@
+const bar = document.getElementsByClassName('explore-img-slider-bar')[0]
+let isDragging = false
+bar.addEventListener('mousedown', (evt) => {
+  bar.style['pointer-events'] = 'none'
+  isDragging = true
+});
+
+const canvas = document.getElementsByClassName('explore-img')[0]
+const r = document.querySelector(':root');
+canvas.addEventListener('mousemove', (evt) => {
+  if (!isDragging) {
+    return
+  }
+  console.log(evt)
+  r.style.setProperty('--image-compare-slider-position', evt.offsetX + 'px');
+})
+
+document.addEventListener('mouseup', () => {
+  isDragging = false
+  bar.style['pointer-events'] = ''
+})
--- museum-dom/video-player.js
+++ museum-dom/video-player.js
@@ -1,3 +1,22 @@
+const progressVideo = document.querySelector('.progress-video');
+
+progressVideo.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #d3d3d3 ${value}%, #d3d3d3 100%)`
+})
+
+const progressVolume = document.querySelector('.progress-volume');
+
+progressVolume.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #d3d3d3 ${value}%, #d3d3d3 100%)`
+})
+
+document.getElementById("menu-toggle").onclick = function () {
+  document.body.classList.toggle("mobile-menu-close")
+  document.body.classList.toggle("mobile-menu-open")
+}
+
 const player = document.querySelector('.player')
 const bigPlayButton = player.querySelector('.player-btn-play-big')
 const video = player.querySelector('video')
