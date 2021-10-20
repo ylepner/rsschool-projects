e9a3275333a04c935b3a0c79bd964e6d669b1e0b
Yulia Lepner <ylepner@gmail.com>
2021-10-20 17:08:52 +0200

    feat: add showTime and showDate fuctions

---
+++ momentum/time.js
@@ -0,0 +1,18 @@
+function showTime() {
+  const time = document.querySelector('.time');
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString('en-GB');
+  time.textContent = currentTime
+  setTimeout(showTime, 1000);
+}
+showTime();
+
+function showDate() {
+  const day = document.querySelector('.date')
+  const date = new Date();
+  const options = { weekday: 'long', day: 'numeric', month: 'long' };
+  const currentDate = date.toLocaleDateString('en-US', options);
+  day.textContent = currentDate
+}
+
+showDate()
