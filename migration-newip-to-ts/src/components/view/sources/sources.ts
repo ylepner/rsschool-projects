4ab36ab81364cd63a389af30e150187d912d3b28
Yulia Lepner <ylepner@gmail.com>
2021-12-03 23:37:38 +0100

    feat: add types to sorces.ts

--- migration-newip-to-ts/src/components/view/sources/sources.ts
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -1,12 +1,22 @@
 import './sources.css';
-
+// интерфейс- как выглядит объект
+interface News {
+  id: string;
+  category: string;
+  country: string;
+  description: string;
+  language: string;
+  name: string;
+  url: string;
+}
 class Sources {
-  draw(data) {
+  draw(data: News[]) {
+    console.log(data);
     const fragment = document.createDocumentFragment();
-    const sourceItemTemp = document.querySelector('#sourceItemTemp');
+    const sourceItemTemp: HTMLTemplateElement = document.querySelector('#sourceItemTemp');
 
     data.forEach((item) => {
-      const sourceClone = sourceItemTemp.content.cloneNode(true);
+      const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
 
       sourceClone.querySelector('.source__item-name').textContent = item.name;
       sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
