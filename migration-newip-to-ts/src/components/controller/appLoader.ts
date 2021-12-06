80cdd1186542ded403bd19b229b416274209a327
Yulia Lepner <ylepner@gmail.com>
2021-12-06 22:31:31 +0100

    fix: input correct url

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://nodenews.herokuapp.com', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: '233320c3d5b94c719b96e0fef48eee07',
     });
   }
