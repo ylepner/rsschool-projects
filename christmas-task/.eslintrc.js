9379865461276ab10be073166eca8e46916b40a3
Yulia Lepner <ylepner@gmail.com>
2021-12-10 23:46:16 +0100

    feat: add start settings

--- christmas-task/.eslintrc.js
+++ christmas-task/.eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
     "import/prefer-default-export": 0,
     "linebreak-style": 0,
     "no-underscore-dangle": 0,
-    "no-param-reassign": 0,
     "class-methods-use-this": 0,
     "react/prop-types": 0,
     "no-restricted-syntax": 0,
