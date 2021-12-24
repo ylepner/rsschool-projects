6fc075f8a5cdc6847d51eb7c9c910d5a89b00eb2
Yulia Lepner <ylepner@gmail.com>
2021-12-24 23:46:47 +0100

    feat: add color lights with css

--- christmas-task/src/components/tree/index.ts
+++ christmas-task/src/components/tree/index.ts
@@ -39,6 +39,8 @@ export function renderTree() {
   template.querySelector('.snow').addEventListener('click', () => {
     snowFalling.classList.toggle('fall');
   });
+  // add lights
+
 
   return template;
 
