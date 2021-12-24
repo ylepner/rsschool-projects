3746ed86634def2bac56c48639df5b82a47da9eb
Yulia Lepner <ylepner@gmail.com>
2021-12-24 17:45:15 +0100

    feat: add onclick to tree option

--- christmas-task/src/components/tree/index.ts
+++ christmas-task/src/components/tree/index.ts
@@ -4,14 +4,18 @@ import './style.css';
 export function renderTree() {
   const template = document.createElement('div');
   template.innerHTML = html;
-  // add tree options
   const treeBox = template.querySelector('.tree-box') as HTMLElement;
+  const treeImg = template.querySelector('.tree-box-img') as HTMLImageElement;
   const audio = template.querySelector('audio');
   const playBtn = template.querySelector('.play-audio') as HTMLElement;
+  // add tree options
   template.querySelectorAll('.tree').forEach((element: HTMLElement, i: number) => {
     i += 1;
     const treeUrl = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/christmas-task/assets/tree/${i}.png`;
     element.style.backgroundImage = `url(${treeUrl})`;
+    element.addEventListener('click', () => {
+      treeImg.src = treeUrl;
+    });
   });
   // add background options
   template.querySelectorAll('.background').forEach((element: HTMLElement, i: number) => {
