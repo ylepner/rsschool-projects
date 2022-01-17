7452c25328f441e3b53fb89766e073660754c5ce
Yulia Lepner <ylepner@gmail.com>
2022-01-17 12:14:43 +0100

    feat: add animation to car

--- async-race/src/components/garage/index.ts
+++ async-race/src/components/garage/index.ts
@@ -4,5 +4,14 @@ import html from './index.html';
 export default function renderGaragePage() {
   const template = document.createElement('div');
   template.innerHTML = html;
+  const carImg = template.querySelector('.car-img') as HTMLImageElement;
+  const startBtn = template.querySelector('.a-btn') as HTMLButtonElement;
+  const restartBtn = template.querySelector('.b-btn') as HTMLButtonElement;
+  startBtn.addEventListener('click', () => {
+    carImg.classList.add('animate');
+  });
+  restartBtn.addEventListener('click', () => {
+    carImg.classList.remove('animate');
+  });
   return template;
 }
