fb91374ce800bdf6dd1a6c2e3e5488e6f19c8464
Yulia Lepner <ylepner@gmail.com>
2022-01-18 14:28:24 +0100

    feat: add car icon in svg

--- async-race/src/components/garage/index.ts
+++ async-race/src/components/garage/index.ts
@@ -4,14 +4,27 @@ import html from './index.html';
 export default function renderGaragePage() {
   const template = document.createElement('div');
   template.innerHTML = html;
-  const carImg = template.querySelector('.car-img') as HTMLImageElement;
+  const carIcon = template.querySelector('svg') as SVGSVGElement;
   const startBtn = template.querySelector('.a-btn') as HTMLButtonElement;
   const restartBtn = template.querySelector('.b-btn') as HTMLButtonElement;
   startBtn.addEventListener('click', () => {
-    carImg.classList.add('animate');
+    carIcon.classList.add('animate');
+    carIcon.onanimationend = () => {
+      console.log('Animation ended');
+    };
   });
   restartBtn.addEventListener('click', () => {
-    carImg.classList.remove('animate');
+    carIcon.classList.remove('animate');
   });
+  const carSVG = template.querySelector('g');
+  carSVG.style.fill = 'orange';
+
+  // fetch('http://localhost:3000/garage', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({ name: 'New car 42', color: '#ff00ff00' }),
+  // });
   return template;
 }
